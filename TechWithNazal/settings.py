de3209b8a296ee7be013a372f2757312a53a9774
CodeWithNazal <codewithnazal@gmail.com>
2023-10-11 19:17:17 +0530

    Media Files To Cloudinary

--- TechWithNazal/settings.py
+++ TechWithNazal/settings.py
@@ -132,7 +132,7 @@ cloudinary.config(
 
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.RawMediaCloudinaryStorage'
