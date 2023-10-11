74ee017cbe73aef19c5dffe3fe5e50ba8ab13b02
CodeWithNazal <codewithnazal@gmail.com>
2023-10-11 19:23:35 +0530

    Media Files To Cloudinary

--- Tuturials/models.py
+++ Tuturials/models.py
@@ -4,7 +4,7 @@ from cloudinary.models import CloudinaryField
 class Tutorial(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True, max_length=200)
-    thumbnail = CloudinaryField('image')
+    thumbnail = CloudinaryField('image', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -22,7 +22,7 @@ class TutorialChapterContent(models.Model):
     chapter = models.ForeignKey(TutorialChapter, on_delete=models.CASCADE)
     title = models.CharField(max_length=100, null=True, blank=True)
     text = models.TextField(null=True, blank=True)
-    image = CloudinaryField('image')
+    image = CloudinaryField('image', null=True, blank=True)
     code_file_name = models.CharField(max_length=30, null=True, blank=True)
     code = models.TextField(null=True, blank=True)
 
@@ -49,7 +49,7 @@ class BlogContent(models.Model):
     blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
     title = models.CharField(max_length=100, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
-    image = CloudinaryField('image')
+    image = CloudinaryField('image', null=True, blank=True)
     code_file_name = models.CharField(max_length=30, null=True, blank=True)
     code = models.TextField(null=True, blank=True)
 
@@ -69,7 +69,7 @@ class NewsContent(models.Model):
     news = models.ForeignKey(News, on_delete=models.CASCADE)
     title = models.CharField(max_length=100, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
-    image = CloudinaryField('image')
+    image = CloudinaryField('image', null=True, blank=True)
 
     def __str__(self):
         return f"{self.title} in {self.news.title}"
